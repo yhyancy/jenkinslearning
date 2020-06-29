572691e0b9b2a3c5ce9a8f86399fbb2e0af43bce
HuaYang <2397856503@qq.com>
2020-06-29 10:17:21 +0800

    first commit

---
+++ src/main/java/com/yh/JenkinslearningApplication.java
@@ -0,0 +1,13 @@
+package com.yh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinslearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinslearningApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yh/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.yh.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+public String helloworld() {
+	return "hello world";
+}
+
+}
