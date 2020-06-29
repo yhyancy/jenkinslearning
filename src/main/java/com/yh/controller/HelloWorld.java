e16aaf754a256491fa0ec247518769795d1769fc
HuaYang <2397856503@qq.com>
2020-06-29 10:57:06 +0800

    add devtools

--- src/main/java/com/yh/controller/HelloWorld.java
+++ src/main/java/com/yh/controller/HelloWorld.java
@@ -1,10 +1,12 @@
 package com.yh.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 	
+@RequestMapping("/hello")
 public String helloworld() {
 	return "hello world";
 }
